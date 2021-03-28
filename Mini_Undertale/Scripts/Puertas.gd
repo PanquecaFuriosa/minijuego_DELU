9f596fbcb8cbcd5a97cdd1b1495f1c122bf81a36
PanquecaFuriosa <18-10761@usb.ve>
2021-03-28 03:51:24 -0400

    Change files

--- Mini_Undertale/Scripts/Puertas.gd
+++ Mini_Undertale/Scripts/Puertas.gd
@@ -1,9 +1,9 @@
 extends Node2D
-signal cambia_scena(value)
+signal cambia_scena(valor)
 
 func _on_Puerta_1_body_entered(body):
 	emit_signal("cambia_scena", 1)
-
+	
 func _on_Puerta_2_body_entered(body):
 	emit_signal("cambia_scena", 2)
 
@@ -11,19 +11,19 @@ func _on_Puerta_3_body_entered(body):
 	emit_signal("cambia_scena", 3)
 
 func _on_Puerta_4_body_entered(body):
-	emit_signal("cambia_scena", 4)
+	emit_signal("cambia_scena", 1)
 
 func _on_Puerta_5_body_entered(body):
-	emit_signal("cambia_scena", 5)
+	emit_signal("cambia_scena", 2)
 
 func _on_Puerta_6_body_entered(body):
-	emit_signal("cambia_scena", 6)
+	emit_signal("cambia_scena", 3)
 
 func _on_Puerta_7_body_entered(body):
-	emit_signal("cambia_scena", 7)
+	emit_signal("cambia_scena", 1)
 
 func _on_Puerta_8_body_entered(body):
-	emit_signal("cambia_scena", 8)
+	emit_signal("cambia_scena", 2)
 
 func _on_Puerta_9_body_entered(body):
-	emit_signal("cambia_scena", 9)
+	emit_signal("cambia_scena", 3)
--- Mini_Undertale/Scripts/main.gd
+++ Mini_Undertale/Scripts/main.gd
@@ -1,5 +1,5 @@
 extends Node2D
+export (PackedScene) var escena
 
-
-func _on_Puertas_cambia_scena(value):
-	pass # Replace with function body
+func _on_Puertas_cambia_scena(valor):
+	get_tree().change_scene_to(escena)
