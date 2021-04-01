8b4dd75a77b68988e455d85ce5fae73677ec700b
Neonhh <thenjhu@gmail.com>
2021-03-31 21:14:28 -0400

    Menu Buttons Work

--- Mini_Undertale/Scripts/Start_Menu.gd
+++ Mini_Undertale/Scripts/Start_Menu.gd
@@ -3,18 +3,30 @@ extends Node2D
 onready var world_button = $"VBoxContainer/HBoxContainer/VBoxContainer/Go to World/mButton"
 onready var battle_button = $"VBoxContainer/HBoxContainer/VBoxContainer/Start Battle/mButton"
 
+export(PackedScene) var mundo
+export(PackedScene) var batalla
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	#Conecta las señales al prersionar los botones para hacer el cambio de escena adecuado
+	
+	world_button.connect("pressed",self, "_On_Button_Press",["world"])
+	battle_button.connect("pressed",self, "_On_Button_Press",["battle"])
 	
 	#Activa el focus en cada boton y enfoca en el primero (ir al mundo)
 	world_button.set_focus_mode(Control.FOCUS_ALL)
 	battle_button.set_focus_mode(Control.FOCUS_ALL)
 	world_button.grab_focus()
-	print(world_button.has_focus())
+
+func _On_Button_Press(value: String):
 	
+	match value:
+		"world":
+			get_tree().change_scene_to(mundo)
+		"battle":
+			get_tree().change_scene_to(batalla)
+		#Por si acaso
+		_:
+			print('Escribiste mal el nombre XD')
+			
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
