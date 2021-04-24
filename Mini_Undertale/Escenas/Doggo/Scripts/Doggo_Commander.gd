70717794e171cb1928408f94562aad6aef53d0a8
Neonhh <thenjhu@gmail.com>
2021-04-24 15:40:30 -0400

    Doggalogs

--- Mini_Undertale/Escenas/Doggo/Scripts/Doggo_Commander.gd
+++ Mini_Undertale/Escenas/Doggo/Scripts/Doggo_Commander.gd
@@ -1,5 +1,18 @@
 extends "res://Commander.gd"
 
+onready var Spawner2 :Position2D = $'Spawner2'
+onready var Spawner3 :Position2D = $'Spawner3'
+onready var Spawner4 :Position2D = $'Spawner4'
+
 func Shoot_Pattern1(what :PackedScene, where: Position2D, reload_time :float = 0, direction: float = 0):
 	where.Fire(what, direction)
 	what.connect('tween_completed',get_parent(),"Start_Enemy_Turn")
+
+#Usado para los dialogos al entrar en estado PET 
+func Dialog_Pattern(what :PackedScene, where: Position2D, reload_time :float = 0, direction: float = 0):
+	pass
+	#Where: un spawner random del 1 al 4
+	#What: un cuadro de dialogo random entre PET, PAT, POT
+	
+	#Se crea sin velocidad, queda testear el reload time (y que al cumplirse el reload time se vuelva a decidir what y where.
+	
--- Mini_Undertale/Escenas/Doggo/Scripts/SpeechGlobe_Doggo.gd
+++ Mini_Undertale/Escenas/Doggo/Scripts/SpeechGlobe_Doggo.gd
@@ -2,5 +2,5 @@ extends "res://SpeechGlobe.gd"
 
 func _ready():
 	._ready()
-	set_speech_dialogs("neutral", ["Did something move?", "I'll make sure it NEVER moves again!"])
+	set_speech_dialogs("neutral", ["Don't move an inch!"])
 	set_speech_dialogs("spare",["WHAT!!! I'VE BEEN PET!!!"])
