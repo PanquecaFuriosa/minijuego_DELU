162b60371d84243549dac9d3219975443b725d48
PanquecaFuriosa <18-10761@usb.ve>
2021-04-25 13:44:28 -0400

    :v

---
+++ Mini_Undertale/Escenas/Doggo/Scripts/Doggo_Spawner.gd
@@ -0,0 +1,16 @@
+extends "res://Spawner.gd"
+
+func Rect_Fire(projectile, direction = -90):
+	#Instancing the projectile
+	var proj = projectile.instance()
+	proj.name = 'projectile'
+	scene.add_child(proj)
+	
+	#Setting position
+	proj.rect_position = get_global_position()  #Aqui esta la diferencia con el original. los TextureRect no tienen position como propiedad. En su lugar tienen rect_position.
+
+	#Setting initial speed
+	#proj.trayecto = Vector2(proj.velocidad,0)
+	#proj.trayecto = proj.trayecto.rotated(direction)
+	proj.set_rotation_degrees(direction)
+	#proj.elegir_trayectoria()
